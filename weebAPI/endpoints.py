a50f96fed9321d848b9ebfbb469c2b86ee217baa
jonnyACCI <jshan9078@gmail.com>
2023-07-30 01:11:56 -0400

    added asynchronous functions

--- weebAPI/endpoints.py
+++ weebAPI/endpoints.py
@@ -14,23 +14,23 @@ async def root():
     return "up and running"
 
 @app.get('/get_search_results/{query}')
-def get_search_results(query: str):
+async def get_search_results(query: str):
     return getSearchResults(session, query)
 
 @app.get('/get_episode/{siteSession}/{epCount}/{episodeNumber}')
-def get_episode(siteSession: str, epCount: int, episodeNumber: int):
+async def get_episode(siteSession: str, epCount: int, episodeNumber: int):
     return getEpisode(session,epCount,siteSession,episodeNumber)
 
 @app.get('/get_download_options/{episodeLink:path}')
-def get_download_options(episodeLink: str):
+async def get_download_options(episodeLink: str):
     return getDownloadOptions(session,episodeLink)
 
 @app.get('/get_download_link/{intermediaryPageLink:path}')
-def get_download_link(intermediaryPageLink):
+async def get_download_link(intermediaryPageLink):
     return getDownloadLink(session,intermediaryPageLink)
 
 @app.get('/get_extra_data/{fullSiteLink:path}')
-def get_extra_data(fullSiteLink):
+async def get_extra_data(fullSiteLink):
     return getExtraData(session,fullSiteLink)
 
 
