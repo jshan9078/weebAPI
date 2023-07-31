408f9e9a336900c5be512082aa3fca4d8dc72aa9
jonnyACCI <jshan9078@gmail.com>
2023-07-31 01:52:13 -0400

    Added descriptions for endpoints

--- weebAPI/endpoints.py
+++ weebAPI/endpoints.py
@@ -7,7 +7,7 @@ from navigator.seriesfinder import getSearchResults, getEpisode, getFullData
 from downloadmanager.downloadscraper import getDownloadLink, getDownloadOptions
 session = requests.session()
 
-app = FastAPI()
+app = FastAPI(title="weebAPI",description="A RESTful API made with FastAPI to scrape anime data from animepahe")
 
 @app.get('/')
 async def root():
@@ -15,22 +15,35 @@ async def root():
 
 @app.get('/get_search_results/{query}')
 async def get_search_results(query: str):
+    """Get search results by typing in the anime you are looking for"""
+
     return getSearchResults(session, query)
 
 @app.get('/get_full_data/{siteLink}')
-async def get_full_data(siteLink):
+async def get_full_data(siteLink: str):
+    """Get complete data for a specific anime by providing the siteLink. \n
+    siteLink refers to the identifier following 'https://animepahe.ru/anime/' """
+
     return getFullData(session,siteLink)
 
 @app.get('/get_episode/{siteLink}/{epPages}/{episodeNumber}')
 async def get_episode(siteLink: str, epPages: int, episodeNumber: int):
+    """Get data for a particular episode. \n
+    epPages can be obtained by using the get_full_data endpoint \n
+    siteLink refers to the identifier following 'https://animepahe.ru/anime/' """
+
     return getEpisode(session,epPages,siteLink,episodeNumber)
 
 @app.get('/get_download_options/{episodeLink:path}')
 async def get_download_options(episodeLink: str):
+    """Get the available download options for a specific episode. \n
+    episodeLink refers to the  complete 'https://animepahe.ru/play/' link for the episode"""
     return getDownloadOptions(session,episodeLink)
 
 @app.get('/get_download_link/{intermediaryPageLink:path}')
-async def get_download_link(intermediaryPageLink):
+async def get_download_link(intermediaryPageLink: str):
+    """Get the link to the download file for a specific episode's download option \n
+    intermediaryPageLink refers to the 'kwik.cx/' link """
     return getDownloadLink(session,intermediaryPageLink)
 
 
